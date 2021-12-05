17468f7983a7e0efb0983c50557e6e1a906aff32
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-05 11:43:17 -0400

    config: Añadido la variable SECRET para usar con jwt y configurado correctamente el cors.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const routes = require('./routes')
+const config = require('./lib/config')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
-app.use(cors())
+app.use(
+  cors({
+    origin: config.cors,
+  })
+)
 app.use(helmet())
 
 app.use('/api', routes)
