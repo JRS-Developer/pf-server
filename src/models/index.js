555e0b1687fae844075b63411d016ad108f3d191
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-06 21:35:34 -0400

    Añadido las relaciones entre los Tasks y las materias y classes

--- src/models/index.js
+++ src/models/index.js
@@ -6,13 +6,25 @@ const Materias = require('./Materias')
 
 // INFO: Relaciones entre las tablas:
 
+// Classes
 // School - Class (one-to-many)
 Schools.hasMany(Classes)
 Classes.belongsTo(Schools)
-
 // Classes - Materias (many-to-many)
 Classes.belongsToMany(Materias, { through: 'materias_classes' })
 Materias.belongsToMany(Classes, { through: 'materias_classes' })
+
+// Tasks
+// Tasks - Notes (one-to-many)
+Task.hasOne(Notes)
+Notes.belongsTo(Task)
+// Tasks - Classes (one-to-many)
+Classes.hasMany(Task)
+Task.belongsTo(Classes)
+// Tasks - Materias (one-to-many)
+Materias.hasMany(Task)
+Task.belongsTo(Materias)
+// TODO: Añadir relaciones de tasks a students y teachers. (many-to-many)
 
 module.exports = {
   Task,
