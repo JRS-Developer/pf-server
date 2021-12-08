1d49e13e795bef63aa7581a40625ef67d7a4c452
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-08 19:25:44 -0400

    Fix: Corregido un error en el archivo de aulas que no se habia exportado el router, y añadido la ruta de autenticacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const tasksRouter = require('./tasks.routes');
 const classesRouter = require('./classes.routes');
 const users = require('./users.routes');
 const aulas = require('./aulas.routes')
+const auth = require('./auth.routes')
 
 router.use('/tasks', tasksRouter);
 router.use('/users', users);
@@ -10,5 +11,6 @@ router.use('/users', users);
 // FIX: Aulas y classes son lo mismo? Si es asi hay que decidir cual dejar y cual descartar
 router.use('/classes', classesRouter);
 router.use('/aulas', aulas)
+router.use('/auth', auth)
 
 module.exports = router
