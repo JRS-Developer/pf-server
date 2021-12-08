5022f6d91eac7534a1033cb2be20562018e32319
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-12-08 01:14:42 -0400

    Fix: Resuelto un error al importar el archivo de schools.routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const tasks = require('./tasks.routes')
 const classes = require('./classes.routes')
 const users = require('./users.routes')
-const schools = require('./classes.routes')
+const schools = require('./schools.routes')
 
 router.use('/tasks', tasks)
 router.use('/users', users)
