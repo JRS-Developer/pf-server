c3edf42683ad6e0aec7da6a825b6fc8a1ce77ac6
Nliasso <nliasso@hotmail.com>
2021-12-16 01:11:25 -0300

    Las rutas de materias estan verificadas y ahora podemos asignar materias a una class

--- src/controllers/materias.controller.js
+++ src/controllers/materias.controller.js
@@ -2,34 +2,43 @@ const Materias = require('../models/Materias')
 const Joi = require('joi')
 
 const getMateriaSchema = Joi.object({
-    id: Joi.string().guid().required(),
     name: Joi.string().allow(''),
   })
   
   const createMateriaSchema = Joi.object({
-    id: Joi.string().guid().required(),
     name: Joi.string().required(),
   })
 
-//get para obtener la materia
-const get_materia = async (req, res, next)=>{
-    try{
-        const {id, name} = req.body
-        
-        const { error } = getMateriaSchema.validate({ id, name })
-        if (error) return res.status(400).json({ error: error.details[0].message })
-        
-        if(id){
-            const findMateria = await Materias.findAll({
-                where:{id, name}
-            })
-            return res.json(findMateria)
-        }else{
-            return res.status(400).json({message: 'This materia do not exist'})
-        }
-    }catch(error) {
-        next(error)
+
+  const get_materias = async (req, res, next) => {
+    try {
+      const materias = await Materias.findAll()
+  
+      //se envia la respuesta como un arreglo de objetos
+      res.json(materias)
+    } catch (error) {
+      console.error(error)
+      next(error)
     }
+  }
+
+
+//get para obtener la materia
+const get_one_materia = async (req, res, next)=>{
+    try{
+        const { id } = req.params
+        
+        const materia_found = await Materias.findByPk(id)
+      
+      
+          //se verifica si se encontró coincidencia y se retorna el objeto sino se envia error
+          if (!materia_found) return res.status(400).json({ error: "No encontramos la materia" })
+      
+          return res.json(materia_found)
+        } catch (error) {
+          console.error(error)
+          next(error)
+        }
 }
 
 //post para crear la materia
@@ -37,13 +46,16 @@ const create_materia = async (req, res, next)=>{
     try{
     const { name } = req.body
     
-    const { error } = createMateriaSchema.validate({ id, name })
+    const { error } = createMateriaSchema.validate({ name })
     if (error) return res.status(400).json({ error: error.details[0].message })
     
-    const materiaFound = await Materias.findByPk(id)
-    if (!materiaFound)return res.status(400).json({ error: 'There is not any materia with that ID' })
+    
+    const [nuevaMateria, created] = await Materias.findOrCreate({
+        where: {name, status:true},
+    })
+    if (!created)return res.status(400).json({ error: 'Ya hay una materia con ese nombre' })
+    
 
-    await Materias.create({name})
     res.json({ message: 'materia successfully created' })
     }catch(error){
         next(error)
@@ -54,7 +66,7 @@ const create_materia = async (req, res, next)=>{
 const delete_materia = async (req, res, next)=>{
     try{
         const { id } = req.params
-        await Materias.destroy({where: {id: id}})
+        await Materias.update({status: false}, {where: {id}})
         res.json({message: 'materia was deleted'})
     }catch(error) {
         next(error)
@@ -62,7 +74,8 @@ const delete_materia = async (req, res, next)=>{
 }
 
 module.exports = {
-    get_materia,
+    get_materias,
+    get_one_materia,
     create_materia,
     delete_materia
 }
