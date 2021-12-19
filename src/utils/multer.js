edfa5377392e9794f80379535922286609cccbaf
Jose Sancheza <sanchezjoserafael25@gmail.com>
2021-12-18 20:01:32 -0400

    Añadido la funcion multiFiles para handlear mas de un archivo con multer

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,18 +1,29 @@
-const multer = require('multer');
+const multer = require('multer')
+const path = require('path')
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'files')
-    },
-    filename: (req, file, cb) => {
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../../files'))
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.originalname.slice(
+        0,
+        file.originalname.indexOf('.')
+      )}-${Date.now()}${file.originalname.slice(
+        file.originalname.indexOf('.')
+      )}`
+    )
+  },
+})
 
-        console.log(file)
-        cb(null, `${file.originalname.slice(0, file.originalname.indexOf('.'))}-${Date.now()}${file.originalname.slice(file.originalname.indexOf('.'))}`);
-    },
-});
-
-const upload = multer({storage});
+const upload = multer({ storage })
 
 const file = upload.single('myFile')
+const multiFiles = upload.array('myFile')
 
-module.exports = file
+module.exports = {
+  file,
+  multiFiles
+}
