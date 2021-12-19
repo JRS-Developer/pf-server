986e01d7e9e03c70cdcd9250d85b4ebc271daf77
Jose Sancheza <sanchezjoserafael25@gmail.com>
2021-12-18 20:33:34 -0400

    Feat: Añadido un comprobante de que la carpeta files existe, si no, pues la crea

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,9 +1,16 @@
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs-extra')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../../files'))
+    const directory = path.join(__dirname, '../../files')
+
+    // Con ensureDir comprueba que la carpeta files exista, si
+    // no existe entonces la crea, asi evitamos errores 500 de parte de multer.
+    fs.ensureDir(directory).then(() => {
+      cb(null, directory)
+    })
   },
   filename: (req, file, cb) => {
     cb(
@@ -25,5 +32,5 @@ const multiFiles = upload.array('myFile')
 
 module.exports = {
   file,
-  multiFiles
+  multiFiles,
 }
