28832558bc583ecba3f400d5ff3868dbf8afc452
Leandro Villafuerte <leandrovillafu@gmail.com>
2021-12-31 16:32:53 -0300

    refact modelo task

--- src/models/Task/Task.js
+++ src/models/Task/Task.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize')
 const { conn: sequelize } = require('../../db')
 const Classes = require('../Classes')
 const Materias = require('../Materias')
-const User = require('../User')
+const Matricula = require('../Matricula')
 const StudentTask = require('./StudentTask')
 
 const Task = sequelize.define('task', {
@@ -15,8 +15,8 @@ const Task = sequelize.define('task', {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  description: DataTypes.STRING,
-  end_date: DataTypes.DATE,
+  description: {type:DataTypes.STRING},
+  end_date: {type:DataTypes.DATE},
 })
 
 // Tasks - Classes (one-to-many)
@@ -27,12 +27,9 @@ Task.belongsTo(Classes, {foreignKey: 'class_id'})
 Materias.hasMany(Task, {foreignKey: 'materia_id'})
 Task.belongsTo(Materias, {foreignKey: 'materia_id'})
 
-// Tasks - Teachers (one-to-many)
-User.hasMany(Task, { foreignKey: 'teacher_id' })
-Task.belongsTo(User, { foreignKey: 'teacher_id' })
 // Tasks - Students (many-to-many)
-User.belongsToMany(Task, { through: StudentTask, foreignKey: 'task_id' })
-Task.belongsToMany(User, { through: StudentTask, foreignKey: 'student_id' })
+Matricula.belongsToMany(Task, { through: StudentTask, foreignKey: 'task_id' })
+Task.belongsToMany(Matricula, { through: StudentTask, foreignKey: 'student_id' })
 
 
 module.exports = Task
