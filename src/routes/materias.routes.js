7e28e8fcd9032d0e8f3bb6807f5329de57150e7d
Nliasso <nliasso@hotmail.com>
2022-01-04 11:45:23 -0300

    arreglos a unas autorizaciones que no estaban funcionando correctamente

--- src/routes/materias.routes.js
+++ src/routes/materias.routes.js
@@ -17,11 +17,9 @@ const {
 router.use(verifyToken)
 
 router.get('/', esSuperUserOrAdmin, get_materias)
-router.get('/:id', esSuperUserOrAdmin, get_one_materia) //sUser, admin y los participantes(profesor participante, verificar q sea profesor en el rol)
+router.get('/:id', esSuperUserOrAdmin, get_one_materia)
 router.post('/', esSuperUserOrAdminOrProfesor, create_materia)
 router.delete('/:id', esSuperUserOrAdmin, delete_materia)
 router.put('/:id', esSuperUserOrAdmin, update_materia)
-
-//crear un put materia y q el profesor lo pueda editar
 
 module.exports = router
