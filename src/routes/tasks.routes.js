805ffd4d409fbcd929b80d5bda008bae9604e552
Luan Klett <luanklett@gmail.com>
2022-01-09 22:06:49 -0300

    Agregadas rutas para subir las tareas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -10,13 +10,15 @@ const {
   alumnoGetTasks,
   profesorUpdateStudentTaskById,
   profesorGetStudentsTask,
+  subirTarea,
+  eliminarFile,
 } = require('../controllers/tasks.controller')
 const {
   verifyToken,
   esProfesor,
   esProfesorOrAlumno,
 } = require('../middlewares/auth')
-
+const { file } = require('../utils/multer')
 router.use(verifyToken)
 
 //ANOTACIÓN: En este paso solo verificamos el role que tenga el usuario
@@ -29,10 +31,12 @@ router.post('/', esProfesor, createTask)
 router.patch('/alumno/:id', esProfesorOrAlumno, changeTaskStatusById)
 router.get('/alumno/:id', esProfesorOrAlumno, alumnoGetTaskById)
 router.get('/alumno', esProfesorOrAlumno, alumnoGetTasks)
+router.post('/alumno', [esProfesorOrAlumno, file], subirTarea)
 // router.get('/:id', esProfesor, getTaskById)
 router.put('/:id', esProfesor, updateTaskById)
 router.put('/:matricula_id/:id', esProfesor, profesorUpdateStudentTaskById)
 router.get('/:id', esProfesorOrAlumno, profesorGetStudentsTask)
 router.delete('/:id', esProfesor, deleteTaskById)
+router.patch('/alumno',esProfesorOrAlumno, eliminarFile) //pongo patch porque si es delete me tira error de permisos (rarisimo)
 
 module.exports = router
---
+++ src/utils/crearRutaDoc.js
@@ -0,0 +1,5 @@
+const crearRuta = (req, filename) => {
+   return `${req.protocol}://${req.get('host')}/files/${filename}`
+}
+
+module.exports = crearRuta
