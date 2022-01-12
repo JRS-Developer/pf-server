335e9e3a67e8372ccbeacb4a81244547caceff97
ferwin <ferwin.arias@gmail.com>
2022-01-12 13:57:14 -0500

    resolve conflicts

--- src/services/webpushConfig.js
+++ src/services/webpushConfig.js
@@ -4,5 +4,6 @@ const { public_vapid_key, private_vapid_key} = require('../lib/config');
 webpush.setVapidDetails('https://localhost:3000', public_vapid_key, private_vapid_key)
 
 
+
 module.exports = webpush
 
